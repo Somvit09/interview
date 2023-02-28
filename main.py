3ba79b8b286cf3eea1b2990f46b738b65de71a53
Somvit09 <developer.09.sam@gmail.com>
2023-02-28 20:19:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def bubblesort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+arr = [4, 6, 8, 2, 3, 1]
+print(bubblesort(arr))
